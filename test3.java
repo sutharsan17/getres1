197ea139bc304ad41c68aff5a52a5c539d319378
sutharsan17 <sutharsanraja1717@gmail.com>
2025-02-22 12:51:35 +0530

    this is the modified

---
+++ test3.java
@@ -0,0 +1,11 @@
+package getres1;
+
+import java.util.Arrays;
+
+public class test3 {
+    public static void main(String[] args) {
+        int[] arr={13,34,23,45};
+        System.out.print(Arrays.toString(arr));
+    }
+    
+}
